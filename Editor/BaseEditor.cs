b60872ce2993a87de8d0d63a87f74e06584682c0
Liam Hoe <liamhoe@hotmail.com>
2023-01-31 15:31:44 +0000

    Added BaseEditor.cs

---
+++ Editor/BaseEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BaseEditor : EditorWindow
+{
+	int value = 0;
+
+	[MenuItem("Animmersion/BaseEditor")]
+	public static void ShowWindow()
+	{
+		GetWindow(typeof(BaseEditor));
+	}
+
+	protected void OnGUI()
+	{
+		if (GUILayout.Button(string.Format("Test Button: {0}", value)))
+		{
+			value++;
+		}
+	}
+}
